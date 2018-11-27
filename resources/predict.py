9009f51860995a60d801cd6a866557f69262d54d
unknown <shikan1998@gmail.com>
2018-11-27 11:14:26 +0530

    added gitignore, displayed tommorow prediction and mean square error

--- resources/predict.py
+++ resources/predict.py
@@ -4,5 +4,8 @@ from models.lstm import LSTM_Model
 
 class Predict(Resource):
     def get(self, name):
-        predict, original = LSTM_Model.LSTM_Pred(name)
-        return {'predicted': '{}'.format(predict), 'original': '{}'.format(original)}, 200
+        predict, original, tomm_pred, mse = LSTM_Model.LSTM_Pred(name)
+        return {'predicted': '{}'.format(predict),
+                'original': '{}'.format(original),
+                'tommrw_prdctn': '{}'.format(tomm_pred),
+                'mn_sqre_err': '{}'.format(mse)}, 200
